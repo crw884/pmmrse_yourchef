2e8c80961a253d9e75f099523427d1d4c984edd3
crw884 <popadkin_mm@edu.surgu.ru>
2025-05-16 05:50:10 +0500

    accounts front

---
+++ accounts/forms.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(label = 'Username', widget=forms.TextInput())
+    email = forms.EmailField(max_length=254)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Password confirm', widget=forms.PasswordInput())
+    username.widget.attrs.update({'class': 'form_username-field','placeholder': 'Имя пользователя'})
+    email.widget.attrs.update({'class': 'form_username-field', 'placeholder': 'Электронная почта'})
+    password1.widget.attrs.update({'class': 'form_username-field', 'placeholder': 'Пароль'})
+    password2.widget.attrs.update({'class': 'form_username-field', 'placeholder': 'Повторите пароль'})
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label='Имя пользователя',widget=forms.TextInput())
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput())
+    username.widget.attrs.update({'placeholder': 'Имя пользователя'})
+    password.widget.attrs.update({'placeholder': 'Пароль'})
---
+++ yourchef/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request, 'homepage.html')
+
+def about(request):
+    return render(request, 'about.html')
